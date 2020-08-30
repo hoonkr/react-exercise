c6c22c1805846d4ac8357b5bd608038de7ce2f96
Hyung Hoon Song <hyunghoonuk@gmail.com>
2020-08-31 00:01:51 +0100

    fetch post comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -22,7 +22,7 @@ import {baseUrl} from '../shared/baseUrl';
     }
     
 
-   function RenderComments({comments, addComment, dishId}){
+   function RenderComments({comments, postComment, dishId}){
         if(comments!=null)
                 return(
                     <div className="col-12 col-md-5 m-1">    
@@ -37,7 +37,7 @@ import {baseUrl} from '../shared/baseUrl';
                                 ); 
                             })}
                         </ul>
-                        <CommentForm dishId = {dishId} addComment = {addComment}/>
+                        <CommentForm dishId = {dishId} postComment = {postComment}/>
                         
                     </div>
                 );
@@ -85,7 +85,7 @@ import {baseUrl} from '../shared/baseUrl';
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                     />
                 </div>
